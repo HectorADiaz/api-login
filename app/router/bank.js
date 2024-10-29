08c75d9a4157b0ae144202a593d95d69b5eb7b64
Hector A Diaz <loginhectordf@gmail.com>
2024-10-28 23:44:40 -0600

    add bank router

--- app/router/bank.js
+++ app/router/bank.js
@@ -1,6 +1,6 @@
 const router = require("express").Router();
 const {Bank} = require("../model/associations");
-
+const AccountType = require("../model/AccountType");
 
 
 router.get("/bank", async (req, res) => {
@@ -19,9 +19,27 @@ router.get("/bank", async (req, res) => {
             message: "An error occurred while retrieving Bankss.",
             error: error.message,
           });        
-    }
+    }   
 });
 
+router.get("/accountType", async (req, res) => {
+    try {
+        const accountType = await AccountType.findAll({
+            attributes: ["accountTypeId","typeName"]
+          });
+        return res.status(200).json({
+            ok:true,
+            data: accountType
+        })
+    } catch (error) {
+        return res.status(500).json({
+            ok: false,
+            status: 500,
+            message: "An error occurred while retrieving Account Type.",
+            error: error.message,
+          });        
+    }   
+});
 
 
 
